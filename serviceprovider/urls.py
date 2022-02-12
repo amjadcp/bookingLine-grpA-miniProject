236234b747813ce4a065737fa491dec90025af29
amjadcp <amjadcp03@gmail.com>
2022-02-12 17:10:34 +0530

    feature:update-auditorium-data

--- serviceprovider/urls.py
+++ serviceprovider/urls.py
@@ -5,5 +5,6 @@ from .views import *
 app_name='serviceprovider'
 urlpatterns = [
     path('add-auditorium', add_auditorium, name='add-auditorium'),
+    path('update-auditorium/<int:id>', update_auditorium, name='update-auditorium'),
     path('auditorium-dashboard/<str:user>/<int:id>', auditorium_dashboard, name='auditorium-dashboard'),
 ]
--- serviceprovider/views.py
+++ serviceprovider/views.py
@@ -55,3 +55,40 @@ def auditorium_dashboard(request, user, id):
     books = Book.objects.filter(auditorium=auditorium)
     context = {'auditorium':auditorium, 'books':books}
     return render(request, 'auditorium_dashboard.html', context=context)
+
+@login_required
+def update_auditorium(request,id):
+    if request.method == 'POST':
+        auditorium = Auditorium.objects.get(id=id)
+        user = request.user
+        auditorium.name = request.POST['name']
+        catelog = request.FILES.get('catelog')
+        if catelog != None:
+            auditorium.catelog = catelog
+        auditorium.about = request.POST['about']
+        auditorium.number = request.POST['number']
+        auditorium.email = request.POST['email']
+        auditorium.price = request.POST['price']
+        auditorium.street = request.POST['street']
+        auditorium.district = request.POST['district']
+        auditorium.state = request.POST['state']
+        auditorium.pin = request.POST['pin']
+        auditorium.post = request.POST['post']
+        auditorium.save()
+        images = request.FILES.getlist('images')
+
+        if images != []:
+            previews = Preview.objects.filter(auditorium=auditorium) 
+            for preview in previews:
+                preview.delete()
+
+            for image in images:
+                Preview.objects.create(
+                    auditorium = auditorium,
+                    images = image
+                )  
+        return redirect(f'../auditorium-dashboard/{request.user}/{id}')
+
+        
+    data = Auditorium.objects.get(id=id)
+    return render(request, 'update_auditorium.html', {'data' : data})
